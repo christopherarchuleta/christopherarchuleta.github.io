d27471a8da25658af34c9b6c50f7f526b3afe610
christopherjarch <archuleta@wisc.edu>
2021-05-11 16:33:46 -0500

    Add Publication

---
+++ Assets/Christopher Archuleta | Maps_files/codes.js
@@ -0,0 +1,23 @@
+// Change color for hover as an affordance to click on maps
+// The second function changes the color
+// back when the pointer leaves the image
+$("img#Image").hover(function() {
+  $(this).css("background-color", "#00884b");
+}, function() {
+  $(this).css("background-color", "#0c2c1e");
+});
+
+
+// Responsive class in topnav makes menu vertically oriented
+// Function below activated the responsive class
+// Expand menu upon icon click
+document.getElementById("icon").onclick = function()
+{verticalMenu()};
+function verticalMenu() {
+  var vertBar = document.getElementById("Navbar");
+  if (vertBar.className === "topnav") {
+    vertBar.className += " responsive";
+  } else {
+    vertBar.className = "topnav";
+  }
+};
