b5c18feb32e2d856d7218115c04f1ef6aa4fed43
Christopher Jerald Archuleta <58175135+christopherarchuleta@users.noreply.github.com>
2024-12-04 10:38:59 -0600

    Use window.onload

--- Codes/codes.js
+++ Codes/codes.js
@@ -1,29 +1,34 @@
-// Change color for hover as an affordance to click on maps
-// The second function changes the color
-// back when the pointer leaves the image
-$("img#Image").hover(function() {
-  $(this).css("background-color", "#00884b");
-}, function() {
-  $(this).css("background-color", "#0c2c1e");
-});
+// Once page and elements load
+window.onload = function() {
+
+  // Change color for hover as an affordance to click on maps
+  // The second function changes the color
+  // back when the pointer leaves the image
+  $("img#Image").hover(function() {
+    $(this).css("background-color", "#00884b");
+  }, function() {
+    $(this).css("background-color", "#0c2c1e");
+  });
 
 
-// Responsive class in topnav makes menu vertically oriented
-// Function below activated the responsive class
-// Expand menu upon icon click
-document.getElementById("icon").onclick = function()
-{verticalMenu()};
-function verticalMenu() {
-  const vertBarList = document.getElementById("NavbarMobile").classList;
-  if (vertBarList.contains('topnav')) {
-    vertBarList.add('responsive');
-  } else {
-    vertBarList.remove('responsive');
-  }
+  // Responsive class in topnav makes menu vertically oriented
+  // Function below activated the responsive class
+  // Expand menu upon icon click
+  document.getElementById("icon").onclick = function()
+  {verticalMenu()};
+  function verticalMenu() {
+    const vertBarList = document.getElementById("NavbarMobile").classList;
+    if (vertBarList.contains('topnav')) {
+      vertBarList.add('responsive');
+    } else {
+      vertBarList.remove('responsive');
+    }
 
-  if (vertBarList.contains('responsive')) {
-    console.log('responsive');
-  } else {
-    ('no');
-  }
+    if (vertBarList.contains('responsive')) {
+      console.log('responsive');
+    } else {
+      ('no');
+    }
+  };
+
 };
